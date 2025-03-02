4201ab4fc575df1f59d27f561728aded4070cf2d
Python_coder_K <turntabroz@gmail.com>
2025-03-03 06:34:04 +0900

    atcoder

---
+++ 0206_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r;
+    r = 3;
+    cout << r * r * 3.14 << "\n";
+    cout << 5 * 3 * 3.14 << "\n";
+    return 0;
+}
---
+++ 0208_1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float base, heght;
+    float area;
+
+    base = 3.0;
+    heght = 3.0;
+
+    area = base * heght / 2.0;
+    printf("Area of a base %f\n", area);
+
+    return 0;
+}
